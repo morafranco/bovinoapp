d718b15b904e3bf7d4448997f83f4d5d718de7c4
Martin Mora <martin@wasp.mx>
2023-10-21 12:47:46 -0600

    wi

---
+++ lib/res/componenets/fields/input_text_password.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [TextField].
+
+class ObscuredTextFieldSample extends StatelessWidget {
+  const ObscuredTextFieldSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 250,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+}
